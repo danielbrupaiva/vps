0f89d2170b2b873a2b3c6e871c7cc38d050cf9af
Daniel <danielbrug@gmail.com>
2025-04-23 02:37:01 +0000

    Add TLS certbot with auto renew

--- nginx-reverse-proxy/entrypoint.sh
+++ nginx-reverse-proxy/entrypoint.sh
@@ -16,6 +16,14 @@ certbot --nginx -d bootoolz.com -d www.bootoolz.com \
 # Start Nginx with the SSL certificates
 nginx -g 'daemon off;'
 
-# Optional: Add Certbot auto-renewal
-# Cron job or systemd timer could be set here if needed
-# (This can also be set inside the container if running as a long-lived service)
+# Optional: Set up Certbot auto-renewal using a cron job
+echo "Setting up Certbot auto-renewal with cron job..."
+
+# Create a cron job for Certbot auto-renewal
+echo "0 0,12 * * * root certbot renew --quiet && systemctl reload nginx" > /etc/cron.d/certbot-renewal
+
+# Make sure the cron job is executable
+chmod +x /etc/cron.d/certbot-renewal
+
+# Start cron daemon (needed to run cron jobs)
+cron
