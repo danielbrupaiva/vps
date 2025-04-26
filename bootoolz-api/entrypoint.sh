c85d78a31d82f902b71ecbba26f10dbdb6c4a9f1
Daniel <danielbrug@gmail.com>
2025-04-26 23:52:59 +0000

    Add dev and prod profile at spring build

--- bootoolz-api/entrypoint.sh
+++ bootoolz-api/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+set -e
+
 echo "Starting Spring Boot app..."
-exec ./gradlew bootRun
+
+if [ "$APP_PROFILE" == "prod" ]; then
+    echo "Running in production mode..."
+    exec ./gradlew bootRun --args='--spring.profiles.active=prod'
+else
+    echo "Running in development mode..."
+    exec ./gradlew bootRun --args='--spring.profiles.active=dev'
+fi
