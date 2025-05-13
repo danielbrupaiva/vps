15d8a800847b18a2f4cdde7b754774878030b6f5
Daniel Paiva <danielbrug@gmail.com>
2025-05-13 20:56:47 -0300

    Add Message object

---
+++ bootoolz-api/src/main/java/com/bootoolz/entities/Message.java
@@ -0,0 +1,24 @@
+package com.bootoolz.entities;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.UUID;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private UUID uuid = UUID.randomUUID();;
+    private LocalDateTime timestamp = LocalDateTime.now(ZoneOffset.UTC);
+    @Getter @Setter
+    private String text;
+
+    public Message(String text) {
+        this.text = text;
+    }
+}
+
+
