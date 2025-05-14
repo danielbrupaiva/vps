f59e55b1906650743110a6461efbd63dd9cf95ac
Daniel Paiva <danielbrug@gmail.com>
2025-05-13 21:30:46 -0300

    Update WebSecurityConfiguration with session control

--- bootoolz-api/src/main/java/com/bootoolz/configurations/WebSecurityConfiguration.java
+++ bootoolz-api/src/main/java/com/bootoolz/configurations/WebSecurityConfiguration.java
@@ -3,6 +3,10 @@ package com.bootoolz.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,8 +22,23 @@ public class WebSecurityConfiguration {
                         .anyRequest().authenticated()
                 )
                 .formLogin(withDefaults())
+                .logout(LogoutConfigurer::permitAll)
+                .sessionManagement(session -> session
+                        .maximumSessions(1)
+                        .expiredUrl("/login?expired=true")
+                )
                 .httpBasic(withDefaults());
         return http.build();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new UserDetailsService() {
+//            @Override
+//            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//                return null;
+//            }
+//        };
+//    }
 
 }
